3eb1497592bbab2525d218fd09ef4f2c89e44277
WinglessNomad <74137476+WinglessNomad@users.noreply.github.com>
2021-05-01 19:29:00 +0200

    first setup

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <footer>
+        <div class="code-link">
+          <p>
+            <a
+              href="https://github.com/WinglessNomad/weather-react-app"
+              class="cl"
+              rel="noopener y
+              noreferrer"
+              target="_blank"
+            >
+              Open-source code
+            </a>{" "}
+            for{" "}
+            <a
+              href="https://www.shecodes.io/"
+              class="cl"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              SheCodes{" "}
+            </a>
+            hosted on{" "}
+            <a
+              href="https://www.netlify.com/"
+              class="cl"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Netlify{" "}
+            </a>
+            by Nina B
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
+}
