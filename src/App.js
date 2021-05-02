2b7545ad4dd2fdce5d0561b503707ed419eec934
WinglessNomad <74137476+WinglessNomad@users.noreply.github.com>
2021-05-02 13:02:40 +0200

    Added searche engine, no CSS yet

--- src/App.js
+++ src/App.js
@@ -1,43 +1,52 @@
 import "./App.css";
+import SearchEngine from "./SearchEngine";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <footer>
-        <div class="code-link">
-          <p>
-            <a
-              href="https://github.com/WinglessNomad/dictionary-project"
-              class="cl"
-              rel="noopener y
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <section className="areaLeft">
+            <SearchEngine />
+          </section>
+          <section className="areaRight"></section>
+        </main>
+        <footer>
+          <div class="code-link">
+            <p>
+              <a
+                href="https://github.com/WinglessNomad/dictionary-project"
+                class="cl"
+                rel="noopener y
               noreferrer"
-              target="_blank"
-            >
-              Open-source code
-            </a>{" "}
-            for{" "}
-            <a
-              href="https://www.shecodes.io/"
-              class="cl"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              SheCodes{" "}
-            </a>
-            hosted on{" "}
-            <a
-              href="https://www.netlify.com/"
-              class="cl"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              Netlify{" "}
-            </a>
-            by Nina B
-          </p>
-        </div>
-      </footer>
+                target="_blank"
+              >
+                Open-source code
+              </a>{" "}
+              for{" "}
+              <a
+                href="https://www.shecodes.io/"
+                class="cl"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                SheCodes{" "}
+              </a>
+              hosted on{" "}
+              <a
+                href="https://www.netlify.com/"
+                class="cl"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                Netlify{" "}
+              </a>
+              by Nina B
+            </p>
+          </div>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/SearchEngine.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+export default function SearchEngine() {
+  let [keyword, setKeyword] = useState(null);
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword}`);
+  }
+  function updateKeyword(event) {
+    setKeyword(event.target.value);
+  }
+  return (
+    <div className="searching">
+      <form className="searchForm" onSubmit={search}>
+        <input
+          type="search"
+          className="searchBar"
+          placeholder="Search"
+          aria-label="Search"
+          onChange={updateKeyword}
+        ></input>
+        <button
+          type="submit"
+          className="btn btn-outline-dark searchButton"
+          aria-label="submit search"
+        >
+          Search
+        </button>
+      </form>
+    </div>
+  );
+}
