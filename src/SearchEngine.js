cccd0a28adfc0494e6bb7192431185e815e4a595
WinglessNomad <74137476+WinglessNomad@users.noreply.github.com>
2021-05-02 19:28:01 +0200

    Added API

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,16 +1,25 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./SearchEngine.css";
 
 export default function SearchEngine() {
   let [keyword, setKeyword] = useState(null);
 
+  function getResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+
+    axios.get(apiUrl).then(getResponse);
   }
+
   function updateKeyword(event) {
     setKeyword(event.target.value);
   }
+
   return (
     <div className="searching">
       <form className="searchForm" onSubmit={search}>
