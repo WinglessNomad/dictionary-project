e080087aba7f3e40ecfc5735831ed596bc31d025
WinglessNomad <74137476+WinglessNomad@users.noreply.github.com>
2021-05-22 16:17:21 +0200

    Added photos API

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,14 +1,20 @@
 import React, { useState } from "react";
 import Feedback from "./Feedback";
+import Photos from "./Photos";
 import axios from "axios";
 import "./SearchEngine.css";
 
 export default function SearchEngine() {
   let [keyword, setKeyword] = useState(null);
   let [feedback, setFeedback] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
   function getResponse(response) {
     setFeedback(response.data[0]);
+  }
+
+  function getPexels(response) {
+    setPhotos(response.data.photos);
   }
 
   function search(event) {
@@ -16,6 +22,12 @@ export default function SearchEngine() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
 
     axios.get(apiUrl).then(getResponse);
+
+    let pexelsKey = "563492ad6f917000010000012d1b75641cd64c7da216ff36595a747a";
+    let pexelsUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let header = { Authorization: `Bearer ${pexelsKey}`};
+
+    axios.get(pexelsUrl, {headers: header}).then(getPexels);
   }
 
   function updateKeyword(event) {
@@ -41,6 +53,7 @@ export default function SearchEngine() {
         </button>
       </form></section>
       <Feedback results={feedback} />
+      <Photos photos={photos}/>
     </div>
   );
 }
